f6cf12e4b2e9f70d179b2a4588953fbabaa8d180
Yoshikawa Teru <teruyamato0731@gmail.com>
2023-07-10 19:03:41 +0900

    chore: C++でビルドする

--- Core/Src/main.cpp
+++ Core/Src/main.cpp
@@ -21,8 +21,8 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-#include "ws2812double.h"
 #include "CAN303x8.h"
+#include "ws2812double.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -65,7 +65,7 @@ static void MX_TIM3_Init(void);
 static void MX_CAN_Init(void);
 static void MX_USART1_UART_Init(void);
 /* USER CODE BEGIN PFP */
-int CAN_read(stm_CAN::CAN_303x8* can,uint8_t* data, stm_CAN::FIFO fifo);
+int CAN_read(stm_CAN::CAN_303x8* can, uint8_t* data, stm_CAN::FIFO fifo);
 void write_PWM(TIM_HandleTypeDef* htim, uint32_t channel1, uint32_t channel2, int16_t val);
 /* USER CODE END PFP */
 
@@ -78,8 +78,7 @@ void write_PWM(TIM_HandleTypeDef* htim, uint32_t channel1, uint32_t channel2, in
   * @brief  The application entry point.
   * @retval int
   */
-int main(void)
-{
+int main(void) {
   /* USER CODE BEGIN 1 */
 
   /* USER CODE END 1 */
@@ -115,7 +114,7 @@ int main(void)
   HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_2);
   HAL_TIMEx_PWMN_Start(&htim1, TIM_CHANNEL_2);
 
-  enum state{
+  enum state {
     STATE_RUNNING,
     STATE_STOPPED,
   } state = STATE_STOPPED;
@@ -140,9 +139,8 @@ int main(void)
   can.subscribe_message(0x01, stm_CAN::ID_type::ext, stm_CAN::Frame_type::data, stm_CAN::FIFO::_0);
   can.subscribe_message(original_id, stm_CAN::ID_type::ext, stm_CAN::Frame_type::data, stm_CAN::FIFO::_0);
 
-  auto process_data = [&](uint8_t* data){
-    switch (data[0])
-      {
+  auto process_data = [&](uint8_t* data) {
+    switch(data[0]) {
       case 0x00:  //stop
         state = state::STATE_STOPPED;
         break;
@@ -154,26 +152,16 @@ int main(void)
         state = state::STATE_STOPPED;
         break;
       case 0x03:  //get original id
-        {
-          uint8_t data_response[4];
-          for( uint8_t i = 0; i < 4; i++){
-            data_response[i] = (original_id >> (i*8)) & 0xFF;
-          }
-          can.send(
-          (data[1]) | (data[2] << 8),
-          stm_CAN::ID_type::std,
-          stm_CAN::Frame_type::data,
-          data_response,
-          4
-          );
+      {
+        uint8_t data_response[4];
+        for(uint8_t i = 0; i < 4; i++) {
+          data_response[i] = (original_id >> (i * 8)) & 0xFF;
         }
-        break;
+        can.send((data[1]) | (data[2] << 8), stm_CAN::ID_type::std, stm_CAN::Frame_type::data, data_response, 4);
+      } break;
       case 0x04:  //set data id
-        can.subscribe_message(
-          (data[1]) | (data[2] << 8),
-          stm_CAN::ID_type::std,
-          stm_CAN::Frame_type::data,
-          stm_CAN::FIFO::_1);
+        can.subscribe_message((data[1]) | (data[2] << 8), stm_CAN::ID_type::std, stm_CAN::Frame_type::data,
+                              stm_CAN::FIFO::_1);
         spnum = data[3];
         break;
       case 0x05:  //set pwm
@@ -182,7 +170,7 @@ int main(void)
 
       default:
         break;
-      }
+    }
     return;
   };
 
@@ -196,50 +184,48 @@ int main(void)
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  while (1)
-  {
-
+  while(1) {
+    uint8_t hoge[] = "hoge\n";
+    HAL_UART_Transmit(&huart1, hoge, 5, 1);
     uint8_t data_command[8];
-    if(CAN_read(&can, data_command, stm_CAN::FIFO::_0)){
+    if(CAN_read(&can, data_command, stm_CAN::FIFO::_0)) {
       process_data(data_command);
     }
     uint8_t data_drive[8];
-    if(CAN_read(&can, data_drive, stm_CAN::FIFO::_1)){
-      output_value = (data_drive[spnum*2] << (spnum*2*8)) | (data_drive[spnum*2 + 1] << (spnum*2*8 + 8));
+    if(CAN_read(&can, data_drive, stm_CAN::FIFO::_1)) {
+      output_value = (data_drive[spnum * 2] << (spnum * 2 * 8)) | (data_drive[spnum * 2 + 1] << (spnum * 2 * 8 + 8));
     }
 
-    switch (state)
-    {
-    case state::STATE_STOPPED:
-      HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, GPIO_PIN_SET);
-      write_PWM(&htim1, TIM_CHANNEL_1, TIM_CHANNEL_2, 0);
-      pixels.colors[0] = _purple;
-      pixels.colors[1] = _purple;
-      pixels.rend();
-      break;
-    case state::STATE_RUNNING:
-      HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, GPIO_PIN_RESET);
-      write_PWM(&htim1, TIM_CHANNEL_1, TIM_CHANNEL_2, output_value);
-      pixels.colors[0] = (output_value > 0)? _blue : (output_value == 0)? _white : _orenge;
-      pixels.colors[1] = _white;
-      pixels.rend();
-      break;
+    switch(state) {
+      case state::STATE_STOPPED:
+        HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, GPIO_PIN_SET);
+        write_PWM(&htim1, TIM_CHANNEL_1, TIM_CHANNEL_2, 0);
+        pixels.colors[0] = _purple;
+        pixels.colors[1] = _purple;
+        pixels.rend();
+        break;
+      case state::STATE_RUNNING:
+        HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, GPIO_PIN_RESET);
+        write_PWM(&htim1, TIM_CHANNEL_1, TIM_CHANNEL_2, output_value);
+        pixels.colors[0] = (output_value > 0) ? _blue : (output_value == 0) ? _white : _orenge;
+        pixels.colors[1] = _white;
+        pixels.rend();
+        break;
     }
     HAL_Delay(1);
 
     //test
 #ifdef debug
-	   [&]() {
-    uint8_t send_data1[] = {test_command};
-    can.send(0x00, stm_CAN::ID_type::std, stm_CAN::Frame_type::data, send_data1, 1);
-    uint8_t send_data2[3] = {0x05, (test_PWM) & 0xff, (test_PWM >> 8) & 0xff};
-    can.send(0x00, stm_CAN::ID_type::std, stm_CAN::Frame_type::data, send_data2, 3);
-     } ();
+    [&]() {
+      uint8_t send_data1[] = {test_command};
+      can.send(0x00, stm_CAN::ID_type::std, stm_CAN::Frame_type::data, send_data1, 1);
+      uint8_t send_data2[3] = {0x05, (test_PWM)&0xff, (test_PWM >> 8) & 0xff};
+      can.send(0x00, stm_CAN::ID_type::std, stm_CAN::Frame_type::data, send_data2, 3);
+    }();
 #endif
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-
   }
   /* USER CODE END 3 */
 }
@@ -248,8 +234,7 @@ int main(void)
   * @brief System Clock Configuration
   * @retval None
   */
-void SystemClock_Config(void)
-{
+void SystemClock_Config(void) {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
   RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
@@ -264,29 +249,25 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
   RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
+  if(HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
     Error_Handler();
   }
 
   /** Initializes the CPU, AHB and APB buses clocks
   */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
-  {
+  if(HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK) {
     Error_Handler();
   }
-  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART1|RCC_PERIPHCLK_TIM1;
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART1 | RCC_PERIPHCLK_TIM1;
   PeriphClkInit.Usart1ClockSelection = RCC_USART1CLKSOURCE_PCLK1;
   PeriphClkInit.Tim1ClockSelection = RCC_TIM1CLK_HCLK;
-  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
-  {
+  if(HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK) {
     Error_Handler();
   }
 }
@@ -296,9 +277,7 @@ void SystemClock_Config(void)
   * @param None
   * @retval None
   */
-static void MX_CAN_Init(void)
-{
-
+static void MX_CAN_Init(void) {
   /* USER CODE BEGIN CAN_Init 0 */
 
   /* USER CODE END CAN_Init 0 */
@@ -318,14 +297,12 @@ static void MX_CAN_Init(void)
   hcan.Init.AutoRetransmission = DISABLE;
   hcan.Init.ReceiveFifoLocked = DISABLE;
   hcan.Init.TransmitFifoPriority = DISABLE;
-  if (HAL_CAN_Init(&hcan) != HAL_OK)
-  {
+  if(HAL_CAN_Init(&hcan) != HAL_OK) {
     Error_Handler();
   }
   /* USER CODE BEGIN CAN_Init 2 */
 
   /* USER CODE END CAN_Init 2 */
-
 }
 
 /**
@@ -333,9 +310,7 @@ static void MX_CAN_Init(void)
   * @param None
   * @retval None
   */
-static void MX_TIM1_Init(void)
-{
-
+static void MX_TIM1_Init(void) {
   /* USER CODE BEGIN TIM1_Init 0 */
 
   /* USER CODE END TIM1_Init 0 */
@@ -354,15 +329,13 @@ static void MX_TIM1_Init(void)
   htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim1.Init.RepetitionCounter = 0;
   htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
-  if (HAL_TIM_PWM_Init(&htim1) != HAL_OK)
-  {
+  if(HAL_TIM_PWM_Init(&htim1) != HAL_OK) {
     Error_Handler();
   }
   sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
   sMasterConfig.MasterOutputTrigger2 = TIM_TRGO2_RESET;
   sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
-  if (HAL_TIMEx_MasterConfigSynchronization(&htim1, &sMasterConfig) != HAL_OK)
-  {
+  if(HAL_TIMEx_MasterConfigSynchronization(&htim1, &sMasterConfig) != HAL_OK) {
     Error_Handler();
   }
   sConfigOC.OCMode = TIM_OCMODE_PWM1;
@@ -372,12 +345,10 @@ static void MX_TIM1_Init(void)
   sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
   sConfigOC.OCIdleState = TIM_OCIDLESTATE_RESET;
   sConfigOC.OCNIdleState = TIM_OCNIDLESTATE_RESET;
-  if (HAL_TIM_PWM_ConfigChannel(&htim1, &sConfigOC, TIM_CHANNEL_1) != HAL_OK)
-  {
+  if(HAL_TIM_PWM_ConfigChannel(&htim1, &sConfigOC, TIM_CHANNEL_1) != HAL_OK) {
     Error_Handler();
   }
-  if (HAL_TIM_PWM_ConfigChannel(&htim1, &sConfigOC, TIM_CHANNEL_2) != HAL_OK)
-  {
+  if(HAL_TIM_PWM_ConfigChannel(&htim1, &sConfigOC, TIM_CHANNEL_2) != HAL_OK) {
     Error_Handler();
   }
   sBreakDeadTimeConfig.OffStateRunMode = TIM_OSSR_DISABLE;
@@ -391,15 +362,13 @@ static void MX_TIM1_Init(void)
   sBreakDeadTimeConfig.Break2Polarity = TIM_BREAK2POLARITY_HIGH;
   sBreakDeadTimeConfig.Break2Filter = 0;
   sBreakDeadTimeConfig.AutomaticOutput = TIM_AUTOMATICOUTPUT_DISABLE;
-  if (HAL_TIMEx_ConfigBreakDeadTime(&htim1, &sBreakDeadTimeConfig) != HAL_OK)
-  {
+  if(HAL_TIMEx_ConfigBreakDeadTime(&htim1, &sBreakDeadTimeConfig) != HAL_OK) {
     Error_Handler();
   }
   /* USER CODE BEGIN TIM1_Init 2 */
 
   /* USER CODE END TIM1_Init 2 */
   HAL_TIM_MspPostInit(&htim1);
-
 }
 
 /**
@@ -407,9 +376,7 @@ static void MX_TIM1_Init(void)
   * @param None
   * @retval None
   */
-static void MX_TIM2_Init(void)
-{
-
+static void MX_TIM2_Init(void) {
   /* USER CODE BEGIN TIM2_Init 0 */
 
   /* USER CODE END TIM2_Init 0 */
@@ -427,8 +394,7 @@ static void MX_TIM2_Init(void)
   htim2.Init.Period = 4294967295;
   htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
-  if (HAL_TIM_IC_Init(&htim2) != HAL_OK)
-  {
+  if(HAL_TIM_IC_Init(&htim2) != HAL_OK) {
     Error_Handler();
   }
   sSlaveConfig.SlaveMode = TIM_SLAVEMODE_RESET;
@@ -436,34 +402,29 @@ static void MX_TIM2_Init(void)
   sSlaveConfig.TriggerPolarity = TIM_INPUTCHANNELPOLARITY_RISING;
   sSlaveConfig.TriggerPrescaler = TIM_ICPSC_DIV1;
   sSlaveConfig.TriggerFilter = 0;
-  if (HAL_TIM_SlaveConfigSynchro(&htim2, &sSlaveConfig) != HAL_OK)
-  {
+  if(HAL_TIM_SlaveConfigSynchro(&htim2, &sSlaveConfig) != HAL_OK) {
     Error_Handler();
   }
   sConfigIC.ICPolarity = TIM_INPUTCHANNELPOLARITY_RISING;
   sConfigIC.ICSelection = TIM_ICSELECTION_DIRECTTI;
   sConfigIC.ICPrescaler = TIM_ICPSC_DIV1;
   sConfigIC.ICFilter = 0;
-  if (HAL_TIM_IC_ConfigChannel(&htim2, &sConfigIC, TIM_CHANNEL_1) != HAL_OK)
-  {
+  if(HAL_TIM_IC_ConfigChannel(&htim2, &sConfigIC, TIM_CHANNEL_1) != HAL_OK) {
     Error_Handler();
   }
   sConfigIC.ICPolarity = TIM_INPUTCHANNELPOLARITY_FALLING;
   sConfigIC.ICSelection = TIM_ICSELECTION_INDIRECTTI;
-  if (HAL_TIM_IC_ConfigChannel(&htim2, &sConfigIC, TIM_CHANNEL_2) != HAL_OK)
-  {
+  if(HAL_TIM_IC_ConfigChannel(&htim2, &sConfigIC, TIM_CHANNEL_2) != HAL_OK) {
     Error_Handler();
   }
   sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
   sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
-  if (HAL_TIMEx_MasterConfigSynchronization(&htim2, &sMasterConfig) != HAL_OK)
-  {
+  if(HAL_TIMEx_MasterConfigSynchronization(&htim2, &sMasterConfig) != HAL_OK) {
     Error_Handler();
   }
   /* USER CODE BEGIN TIM2_Init 2 */
 
   /* USER CODE END TIM2_Init 2 */
-
 }
 
 /**
@@ -471,9 +432,7 @@ static void MX_TIM2_Init(void)
   * @param None
   * @retval None
   */
-static void MX_TIM3_Init(void)
-{
-
+static void MX_TIM3_Init(void) {
   /* USER CODE BEGIN TIM3_Init 0 */
 
   /* USER CODE END TIM3_Init 0 */
@@ -491,38 +450,32 @@ static void MX_TIM3_Init(void)
   htim3.Init.Period = 89;
   htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim3.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
-  if (HAL_TIM_Base_Init(&htim3) != HAL_OK)
-  {
+  if(HAL_TIM_Base_Init(&htim3) != HAL_OK) {
     Error_Handler();
   }
   sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
-  if (HAL_TIM_ConfigClockSource(&htim3, &sClockSourceConfig) != HAL_OK)
-  {
+  if(HAL_TIM_ConfigClockSource(&htim3, &sClockSourceConfig) != HAL_OK) {
     Error_Handler();
   }
-  if (HAL_TIM_PWM_Init(&htim3) != HAL_OK)
-  {
+  if(HAL_TIM_PWM_Init(&htim3) != HAL_OK) {
     Error_Handler();
   }
   sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
   sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
-  if (HAL_TIMEx_MasterConfigSynchronization(&htim3, &sMasterConfig) != HAL_OK)
-  {
+  if(HAL_TIMEx_MasterConfigSynchronization(&htim3, &sMasterConfig) != HAL_OK) {
     Error_Handler();
   }
   sConfigOC.OCMode = TIM_OCMODE_PWM1;
   sConfigOC.Pulse = 0;
   sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
   sConfigOC.OCFastMode = TIM_OCFAST_ENABLE;
-  if (HAL_TIM_PWM_ConfigChannel(&htim3, &sConfigOC, TIM_CHANNEL_4) != HAL_OK)
-  {
+  if(HAL_TIM_PWM_ConfigChannel(&htim3, &sConfigOC, TIM_CHANNEL_4) != HAL_OK) {
     Error_Handler();
   }
   /* USER CODE BEGIN TIM3_Init 2 */
 
   /* USER CODE END TIM3_Init 2 */
   HAL_TIM_MspPostInit(&htim3);
-
 }
 
 /**
@@ -530,9 +483,7 @@ static void MX_TIM3_Init(void)
   * @param None
   * @retval None
   */
-static void MX_USART1_UART_Init(void)
-{
-
+static void MX_USART1_UART_Init(void) {
   /* USER CODE BEGIN USART1_Init 0 */
 
   /* USER CODE END USART1_Init 0 */
@@ -550,22 +501,18 @@ static void MX_USART1_UART_Init(void)
   huart1.Init.OverSampling = UART_OVERSAMPLING_16;
   huart1.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
   huart1.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
-  if (HAL_UART_Init(&huart1) != HAL_OK)
-  {
+  if(HAL_UART_Init(&huart1) != HAL_OK) {
     Error_Handler();
   }
   /* USER CODE BEGIN USART1_Init 2 */
 
   /* USER CODE END USART1_Init 2 */
-
 }
 
 /**
   * Enable DMA controller clock
   */
-static void MX_DMA_Init(void)
-{
-
+static void MX_DMA_Init(void) {
   /* DMA controller clock enable */
   __HAL_RCC_DMA1_CLK_ENABLE();
 
@@ -573,7 +520,6 @@ static void MX_DMA_Init(void)
   /* DMA1_Channel3_IRQn interrupt configuration */
   HAL_NVIC_SetPriority(DMA1_Channel3_IRQn, 0, 0);
   HAL_NVIC_EnableIRQ(DMA1_Channel3_IRQn);
-
 }
 
 /**
@@ -581,11 +527,10 @@ static void MX_DMA_Init(void)
   * @param None
   * @retval None
   */
-static void MX_GPIO_Init(void)
-{
+static void MX_GPIO_Init(void) {
   GPIO_InitTypeDef GPIO_InitStruct = {0};
-/* USER CODE BEGIN MX_GPIO_Init_1 */
-/* USER CODE END MX_GPIO_Init_1 */
+  /* USER CODE BEGIN MX_GPIO_Init_1 */
+  /* USER CODE END MX_GPIO_Init_1 */
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOF_CLK_ENABLE();
@@ -602,32 +547,34 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(DISABLE_GPIO_Port, &GPIO_InitStruct);
 
-/* USER CODE BEGIN MX_GPIO_Init_2 */
-/* USER CODE END MX_GPIO_Init_2 */
+  /* USER CODE BEGIN MX_GPIO_Init_2 */
+  /* USER CODE END MX_GPIO_Init_2 */
 }
 
 /* USER CODE BEGIN 4 */
-int CAN_read(stm_CAN::CAN_303x8* can, uint8_t* data, stm_CAN::FIFO fifo){
+int CAN_read(stm_CAN::CAN_303x8* can, uint8_t* data, stm_CAN::FIFO fifo) {
   uint8_t received[] = "received ";
   uint8_t cr_lf[] = "\r\n";
   uint8_t err[] = "error\r\n";
-	switch (can->read(fifo, data)){
-	      case stm_CAN::read_retval::message_received:
-	      case stm_CAN::read_retval::more_message_received:
-	        HAL_UART_Transmit(&huart1, received, 9, 1);
-	        HAL_UART_Transmit(&huart1, data, 8, 1);
-	        HAL_UART_Transmit(&huart1, cr_lf, 2, 1);
-	        return 1;
-	      case stm_CAN::read_retval::error:
-	    	  HAL_UART_Transmit(&huart1, err, 7,1);
-	      case stm_CAN::read_retval::no_message:
-	      default:
-	        return 0;
-	}
+  switch(can->read(fifo, data)) {
+    case stm_CAN::read_retval::message_received:
+    case stm_CAN::read_retval::more_message_received:
+      HAL_UART_Transmit(&huart1, received, 9, 1);
+      HAL_UART_Transmit(&huart1, data, 8, 1);
+      HAL_UART_Transmit(&huart1, cr_lf, 2, 1);
+      return 1;
+    case stm_CAN::read_retval::error:
+      HAL_UART_Transmit(&huart1, err, 7, 1);
+    case stm_CAN::read_retval::no_message:
+    default:
+      return 0;
+  }
 }
 
-void write_PWM(TIM_HandleTypeDef* htim, uint32_t channel1, uint32_t channel2, int16_t val){
-  auto clamp_unsigned = [](int16_t v){return (v > 0)? v : 0;};
+void write_PWM(TIM_HandleTypeDef* htim, uint32_t channel1, uint32_t channel2, int16_t val) {
+  auto clamp_unsigned = [](int16_t v) {
+    return (v > 0) ? v : 0;
+  };
   __HAL_TIM_SET_COMPARE(htim, channel1, clamp_unsigned(val) >> 3);
   __HAL_TIM_SET_COMPARE(htim, channel2, clamp_unsigned(-val) >> 3);
   return;
@@ -638,18 +585,15 @@ void write_PWM(TIM_HandleTypeDef* htim, uint32_t channel1, uint32_t channel2, in
   * @brief  This function is executed in case of error occurrence.
   * @retval None
   */
-void Error_Handler(void)
-{
+void Error_Handler(void) {
   /* USER CODE BEGIN Error_Handler_Debug */
   /* User can add his own implementation to report the HAL error return state */
   __disable_irq();
-  while (1)
-  {
-  }
+  while(1) {}
   /* USER CODE END Error_Handler_Debug */
 }
 
-#ifdef  USE_FULL_ASSERT
+#ifdef USE_FULL_ASSERT
 /**
   * @brief  Reports the name of the source file and the source line number
   *         where the assert_param error has occurred.
@@ -657,8 +601,7 @@ void Error_Handler(void)
   * @param  line: assert_param error line source number
   * @retval None
   */
-void assert_failed(uint8_t *file, uint32_t line)
-{
+void assert_failed(uint8_t* file, uint32_t line) {
   /* USER CODE BEGIN 6 */
   /* User can add his own implementation to report the file name and line number,
      ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
