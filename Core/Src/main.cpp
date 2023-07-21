4be5abc661eda13c563ec906549a2f3858a33b2c
Yoshikawa Teru <teruyamato0731@gmail.com>
2023-07-21 15:52:19 +0900

    fix: CAN Modeをloopbackからnormalに修正

--- Core/Src/main.cpp
+++ Core/Src/main.cpp
@@ -308,7 +308,7 @@ static void MX_CAN_Init(void) {
   /* USER CODE END CAN_Init 1 */
   hcan.Instance = CAN;
   hcan.Init.Prescaler = 2;
-  hcan.Init.Mode = CAN_MODE_LOOPBACK;
+  hcan.Init.Mode = CAN_MODE_NORMAL;
   hcan.Init.SyncJumpWidth = CAN_SJW_1TQ;
   hcan.Init.TimeSeg1 = CAN_BS1_14TQ;
   hcan.Init.TimeSeg2 = CAN_BS2_3TQ;
